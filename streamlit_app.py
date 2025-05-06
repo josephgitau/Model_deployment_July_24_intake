9322c393b17614b2982c4b65626a7eddcfb0e6ac
Joseph Gitau <gitaujoseph64@gmail.com>
2025-05-06 17:41:08 +0000

    Minor update

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ st.title("July Intake Model Deployment")
 ## Add Header
 st.header("Instructure: Joseph Gitau")
 st.text("Hello everyone, My name is Joseph Gitau, and I will be taking you through the model deployment class in the next few days")
+
+st.markdown("Welcome to the course, Hope you will Enjoy")
